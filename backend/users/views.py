02f504556c3a8af2bb1f05cf47d634c41e08ff62
Karina Rin <karishka.eo@yandex.ru>
2023-09-28 22:52:46 +0300

    отмена

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,1 @@
 # backend/users/views.py
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-
-
-@api_view(["POST"])
-@permission_classes([AllowAny])
-def signup(request):
-    """Регистрация пользователя."""
-    pass
