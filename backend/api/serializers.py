a0f6e49a4ee2e3298d15bf901d7a5cc306a4ffc0
Karina Rin <karishka.eo@yandex.ru>
2023-09-29 22:55:49 +0300

    swagger

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -12,28 +12,34 @@ from rest_framework import serializers
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = "__all__"
 
 
 class SaleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = SaleProduct
+        fields = "__all__"
 
 
 class ShopsProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShopsProduct
+        fields = "__all__"
 
 
 class ForecastProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ForecastProduct
+        fields = "__all__"
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
+        fields = "__all__"
