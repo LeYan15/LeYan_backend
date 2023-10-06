64a7c8df33737f338de6cb4d950b1c8cfb73cf78
Karina Rin <karishka.eo@yandex.ru>
2023-10-06 18:10:53 +0300

    settings

--- backend/leyan/settings.py
+++ backend/leyan/settings.py
@@ -118,7 +118,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "back-static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "back-static")
 
