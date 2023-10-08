49258b59a17a270f109e47dc31cadb654f48a6c9
Karina Rin <karishka.eo@yandex.ru>
2023-10-08 19:11:48 +0300

    tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -21,5 +21,7 @@ class TestAuthUrls:
     ])
     def test_auth_urls(self, client, url):
         response = client.get(url)
-        assert response.status_code != 404, f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*'
-        assert response.status_code == 200, f'Ошибка {response.status_code} при открытиии `{url}`. Проверьте ее view-функцию'
+        assert response.status_code != 404, \
+            f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*'
+        assert response.status_code == 200, \
+            f'Ошибка {response.status_code} при открытиии `{url}`. Проверьте ее view-функцию'
