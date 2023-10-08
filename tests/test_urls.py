de0abde3dd4cc6f432f5c58607829e90fb8bf8b3
Karina Rin <karishka.eo@yandex.ru>
2023-10-08 18:41:36 +0300

    test_urls.py дополнение

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,17 +1,25 @@
 import pytest
 
 
+@pytest.mark.django_db(transaction=True)
 class TestAuthUrls:
 
-    @pytest.mark.django_db(transaction=True)
-    def test_auth_urls(self, client):
-        urls = ['/swagger/', '/auth/', '/auth/users/', '/api/', '/api/shops/', '/api/forecast/', '/api/products/', '/api/sales/', '/redoc/']
-        for url in urls:
-            try:
-                response = client.get(url)
-            except Exception as e:
-                assert False, f'''Страница `{url}` работает неправильно. Ошибка: `{e}`'''
-            assert response.status_code != 404, f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*'
-            assert response.status_code == 200, (
-                f'Ошибка {response.status_code} при открытиии `{url}`. Проверьте ее view-функцию'
-            )
+    @pytest.fixture
+    def client(self, client):
+        return client
+
+    @pytest.mark.parametrize('url', [
+        '/swagger/',
+        '/auth/',
+        '/auth/users/',
+        '/api/',
+        '/api/shops/',
+        '/api/forecast/',
+        '/api/products/',
+        '/api/sales/',
+        '/redoc/'
+    ])
+    def test_auth_urls(self, client, url):
+        response = client.get(url)
+        assert response.status_code != 404, f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*'
+        assert response.status_code == 200, f'Ошибка {response.status_code} при открытиии `{url}`. Проверьте ее view-функцию'
