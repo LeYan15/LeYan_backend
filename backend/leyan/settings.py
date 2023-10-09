f6fa20cc3d7a51802def3ab6f21ba47ffa2d97e0
Karina Rin <karishka.eo@yandex.ru>
2023-10-09 20:36:02 +0300

    #

--- backend/leyan/settings.py
+++ backend/leyan/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "templates")],
-        #"DIRS": [],
+        # "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
